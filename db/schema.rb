7ae8c006c3fdb54981419f70d5ed1741d20a2dae
Adrian Knoblauch <adrianknoblauch@me.com>
2017-04-06 21:30:20 +0200

    removed initial collaborator

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406183722) do
-
-  create_table "collaborators", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170329193207) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
